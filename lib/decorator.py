125d587e3077af49efc1b6d43abde2fb3fc91cd5
Changdae <cd.jeong@samsung.com>
2025-11-06 13:28:07 +0900

    decorator: add require_debug_mode
    Signed-off-by: Changdae <cd.jeong@samsung.com>

--- lib/decorator.py
+++ lib/decorator.py
@@ -8,3 +8,12 @@ def non_static_request(f):
             return
         return f(*args, **kwargs)
     return decorated_function
+
+def require_debug_mode(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        from flask import current_app
+        if not current_app.config.get('DEBUG', False):
+            return "This feature is only available in DEBUG mode.", 403
+        return f(*args, **kwargs)
+    return decorated_function
